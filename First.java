3dfa4f4f2b199ad5f79f27192b6b8f651ebf60e6
SohamJagtap1999 <132488130+SohamJagtap1999@users.noreply.github.com>
2025-08-01 18:11:13 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello all");
+        System.out.println("New Changes Made From The GitHub");
     }
 }
